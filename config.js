317b3a7339823bb52276171e0dedee6098a0a1b8
Tim Gehrsitz <36209570+timgehr@users.noreply.github.com>
2024-07-01 20:00:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    db_conn_string: "postgres://postgres:beppy0101@localhost:5432/postgres",
+    db_conn_string: "",
     server_endpoint: "http://localhost:8080",
     uh: 'uh'
 }
